3f0ba3efd8eb91b5e06128531dcba222cdcb2777
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-10-22 00:43:18 -0400

    testing

--- js/script.js
+++ js/script.js
@@ -16,17 +16,17 @@ $(function() {
 });
 
 
-let btnAdd = document.querySelector('#add');
-let btnSubtract = document.querySelector('#subtract');
-let input = document.querySelector('#orderInput');
+// let btnAdd = document.querySelector('#add');
+// let btnSubtract = document.querySelector('#subtract');
+// let input = document.querySelector('#orderInput');
 
-btnAdd.addEventListener('click', () => {
-    input.value = parseInt(input.value) + 1;
-});
-btnSubtract.addEventListener('click', () => {
-    if(input.value === '0') {
-        input.value = '0';
-    } else 
-    input.value = parseInt(input.value) - 1;
-});
+// btnAdd.addEventListener('click', () => {
+//     input.value = parseInt(input.value) + 1;
+// });
+// btnSubtract.addEventListener('click', () => {
+//     if(input.value === '0') {
+//         input.value = '0';
+//     } else 
+//     input.value = parseInt(input.value) - 1;
+// });
 
